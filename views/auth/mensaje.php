161fad2f17b2d36ffe24fcbe7e4fb66a807da6ba
Onofre Davila Morillo <66891673+OnofreDavila@users.noreply.github.com>
2025-05-12 21:57:54 -0400

    Add files via upload

---
+++ views/auth/mensaje.php
@@ -0,0 +1,3 @@
+<h1 class="nombre-pagina">Confirmar tu cuenta</h1>
+
+<p class= "descripcion-pagina" >Hemos enviado las instrucciones para confirmar la creacion de tu cuenta a tu  direccion de e-mail.</p>
