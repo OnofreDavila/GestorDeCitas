f22bab3dbe227a66d89c3b7870960942085f2313
Onofre Davila Morillo <66891673+OnofreDavila@users.noreply.github.com>
2025-05-12 21:54:05 -0400

    Add files via upload

---
+++ classes/Email.php
@@ -0,0 +1,82 @@
+<?php 
+
+namespace Classes;
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+class Email {
+
+    public $email;
+    public $nombre;
+    public $token;
+
+    public function __construct($email, $nombre, $token) {
+        $this->email = $email;
+        $this->nombre = $nombre;
+        $this->token = $token;
+    }
+
+    public function enviarConfirmacion() {
+
+        //Crear el objeto de email
+        $mail = new PHPMailer();
+
+        //protocolo de envio email, configuracion del servidor
+        $mail->isSMTP();
+        $mail->Host = 'sandbox.smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username = 'd2dfcab7a3cb46';
+        $mail->Password = '267b9e3e0dc82f';
+
+        //Recipiente, dominio del proyecto que debo modificar al entrar al hosting 
+        $mail->setFrom('onofrej14@gmail.com');
+        $mail->addAddress('onofrej14@gmail.com', 'Onofre Davila');
+        $mail->Subject = 'Confirma tu cuenta';
+
+        //Set HTML
+        $mail->isHTML(TRUE);
+        $mail->CharSet = 'UFT-8';
+
+        $contenido = "<html>";
+        $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has creado tu cuenta en Peluqueria Canina, solo debes confirmarla presionando el siguiente enlace </p>";
+        $contenido .= "<p>Presionar aqui: <a href='http://localhost:3000/confirmar-cuenta?token=". $this->token ."'>Confirmar Cuenta</a> </p>";
+        $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje.</p>";
+        $contenido .= "</html>";
+        $mail->Body = $contenido;
+
+        //enviar el mail
+        $mail->send();
+    }
+
+    public function enviarInstrucciones() {
+
+        // create a new object
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = 'smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username = 'd2dfcab7a3cb46';
+        $mail->Password = '267b9e3e0dc82f';
+    
+        $mail->setFrom('onofrej14@gmail.com');
+        $mail->addAddress('onofrej14@gmail.com', 'Onofre Davila');
+        $mail->Subject = 'Reestablece tu password';
+
+        // Set HTML
+        $mail->isHTML(TRUE);
+        $mail->CharSet = 'UTF-8';
+
+        $contenido = '<html>';
+        $contenido .= "<p><strong>Hola " . $this->nombre .  "</strong> Has solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo.</p>";
+        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/recuperar?token=" . $this->token . "'>Reestablecer Password</a>";        
+        $contenido .= "<p>Si tu no solicitaste este cambio, puedes ignorar el mensaje</p>";
+        $contenido .= '</html>';
+        $mail->Body = $contenido;
+
+            //Enviar el mail
+        $mail->send();
+    }
+
+}
