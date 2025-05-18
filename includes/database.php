46312f747cafe20b7fefc91734fcfbcf75a5e310
OnofreDavila <onofrej14@gmail.com>
2025-05-18 13:53:07 -0400

    actualizando url de la imagen

--- includes/database.php
+++ includes/database.php
@@ -3,9 +3,10 @@
 $db = mysqli_connect('mysql-peluqueriacanina.alwaysdata.net', '413715', 'bianca14*', 'peluqueriacanina_mvc');
 
 $db->set_charset("utf8");
+
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
-    echo "error de depuración: " . mysqli_connect_errno();
+    echo "error de depuración: " . mysqli_connect_error();
     echo "error de depuración: " . mysqli_connect_error();
     exit;
 }
