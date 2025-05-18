fefb2f0370d459aea98a84f381ea170348a3d48b
OnofreDavila <onofrej14@gmail.com>
2025-05-18 15:50:47 -0400

    corrigiendo el link de verificacion del correo para recibir token

--- classes/Email.php
+++ classes/Email.php
@@ -40,7 +40,7 @@ class Email {
 
         $contenido = "<html>";
         $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has creado tu cuenta en Peluqueria Canina, solo debes confirmarla presionando el siguiente enlace </p>";
-        $contenido .= "<p>Presionar aqui: <a href='http://mysql-peluqueriacanina.alwaysdata.net/confirmar-cuenta?token=". $this->token ."'>Confirmar Cuenta</a> </p>";
+        $contenido .= "<p>Presionar aqui: <a href='http://peluqueriacanina.alwaysdata.net/confirmar-cuenta?token=". $this->token ."'>Confirmar Cuenta</a> </p>";
         $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje.</p>";
         $contenido .= "</html>";
         $mail->Body = $contenido;
@@ -72,7 +72,7 @@ class Email {
 
         $contenido = '<html>';
         $contenido .= "<p><strong>Hola " . $this->nombre .  "</strong> Has solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo.</p>";
-        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/recuperar?token=" . $this->token . "'>Reestablecer Password</a>";        
+        $contenido .= "<p>Presiona aquí: <a href='http://peluqueriacanina.alwaysdata.net/recuperar?token=" . $this->token . "'>Reestablecer Password</a>";        
         $contenido .= "<p>Si tu no solicitaste este cambio, puedes ignorar el mensaje</p>";
         $contenido .= '</html>';
         $mail->Body = $contenido;
